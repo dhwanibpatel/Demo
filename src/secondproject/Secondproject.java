4c1d8bba128bfd410e6e388e4cd8622dc8d72c8a
patel <patel@10.48.49.88>
2023-05-18 15:25:35 -0400

    my first commit

---
+++ src/secondproject/Secondproject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package secondproject;
+
+/**
+ *
+ * @author patel
+ */
+public class Secondproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int a=5, b=8, c=6;
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.min(c, b));
+    }
+    
+}
